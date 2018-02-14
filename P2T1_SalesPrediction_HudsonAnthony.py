dd19a74b61b5d8d61f6c27d85459554fcda0a05a
lamar6332 <35572844+lamar6332@users.noreply.github.com>
2018-02-14 13:57:53 -0500

    Initial submission
    Initial submission for P2T1_SalesPrediction

---
+++ P2T1_SalesPrediction_HudsonAnthony.py
@@ -0,0 +1,17 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Anthony Hudson, Jr.
+# 13 FEB. 2018
+#
+
+#Enter the total amount of sales.
+totalSales = float ( input("What is the projected amount of total sales this year?"))
+
+#Enter the percentage of annual profit.
+percentageProfit = float ( input("Enter the percentage of annual profit:"))
+
+#Calculate the amount of profit made.
+annualProfit = totalSales * percentageProfit
+
+#Display the amount of profit made.
+print ("Congratulations! This is your profit this year:",annualProfit)
