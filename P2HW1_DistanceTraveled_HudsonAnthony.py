7d2e1048c632fd42377838b81905f04c3c322de8
lamar6332 <35572844+lamar6332@users.noreply.github.com>
2018-02-14 14:02:16 -0500

    DistanceTraveled
    Initial submission of DistanceTraveled

---
+++ P2HW1_DistanceTraveled_HudsonAnthony.py
@@ -0,0 +1,23 @@
+# CTI-110 
+# P2HW1 - Distance Traveled  
+# Anthony Hudson, Jr.
+# 13 FEB. 2018
+#
+
+#Speed of all vehicles.
+speed = 70
+
+#The amount of time the vehicles traveled.
+time6 = 6
+time10 = 10
+time15 = 15
+
+#Distance calculation.
+distance6 = speed * time6
+distance10 = speed * time10
+distance15 = speed * time15
+
+#Diplay the distance traveled for each vehicle.
+print ("The vehicle that traveled for six hours covered a distance of:",distance6)
+print ("The vehicle that traveled for ten hours covered a distance of:",distance10)
+print ("The vehicle that traveled for fifteen hours covered a distance of:",distance15)
