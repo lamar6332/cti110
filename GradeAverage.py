74a32215320a733268092470e837526c82a7d887
lamar6332 <35572844+lamar6332@users.noreply.github.com>
2018-02-20 10:41:17 -0500

    Grade Average
    Grade Average

---
+++ GradeAverage.py
@@ -0,0 +1,22 @@
+# CTI-110 
+# Python 2 Excercise 
+# Anthony Hudson, Jr.
+# 20 FEB. 2018
+#
+
+#Fisrt grade
+firstGrade = float ( input("What is your 1st grade? "))
+
+#Second Grade
+secGrade = float ( input("What is your 2nd grade? "))
+
+#Third Grade
+thirdGrade = float ( input("What is your 3rd grade? "))
+
+#Calculate
+average = (firstGrade + secGrade + thirdGrade) /3
+
+#Average
+print ("This is your average:", format (average, ".2f"))
+
+
