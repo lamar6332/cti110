a99948c889d5d41c104ed0808ecf868a0fbc5724
lamar6332 <35572844+lamar6332@users.noreply.github.com>
2018-02-20 10:40:51 -0500

    Discount Calculation
    Discount Calculation

---
+++ DiscountCalc.py
@@ -0,0 +1,18 @@
+# CTI-110 
+# Python 2 Excercise
+# Anthony Hudson, Jr.
+# 20 FEB. 2018
+#
+
+#Obtain price of item.
+itemPrice = float ( input("Enter the price of the item:"))
+
+#Discount.
+discount = itemPrice * .20
+
+#Calculate price of discount and total price.
+totalPrice = itemPrice - discount
+
+#Display the total cost of items with discount.
+print ("The cost of the item with discount is:",totalPrice)
+
